650bc202c3070409c82d9aba8c80a65c3b4fb666
Drew Marshall <43978216+stinklewinks@users.noreply.github.com>
2024-12-13 16:10:28 -0600

    Progress Steps 0.0.1

---
+++ script.js
@@ -0,0 +1,28 @@
+const progress = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progress-container');
+const prev = document.querySelector('#back');
+const next = document.querySelector('#next');
+const steps = document.querySelectorAll('.step');
+let currentActive = 1;
+
+next.addEventListener('click', () => {
+    currentActive++;
+    let currentStep = document.querySelector('.step.active');
+    let nextStep = currentStep.nextElementSibling;
+    if (nextStep) {
+        currentStep.classList.remove('active');
+        nextStep.classList.add('active');
+        progress.style.width = `${(currentActive / steps.length) * 100}` + '%';
+    }
+});
+
+prev.addEventListener('click', () => {
+    currentActive--;
+    let currentStep = document.querySelector('.step.active');
+    let prevStep = currentStep.previousElementSibling;
+    if (prevStep) {
+        currentStep.classList.remove('active');
+        prevStep.classList.add('active');
+        progress.style.width = `${((currentActive - 1) / (steps.length - 1)) * 100}` + '%';
+    }
+});
